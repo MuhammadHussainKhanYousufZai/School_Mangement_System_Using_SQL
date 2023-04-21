88aa1c40b1102ad45727116522b16811c8b35f50
Muhammad Hussain Khan <52101402+hussain2109414@users.noreply.github.com>
2023-04-21 08:18:56 +0500

    Update School Mangement.sql
    Insert the data and clean the data

--- School_Mangement_System/School_Mangement.sql
+++ School_Mangement_System/School_Mangement.sql
@@ -55,3 +55,51 @@ select * from Grades;
 select * from Attendance;
 
 
+TRUNCATE TABLE Student;
+TRUNCATE TABLE Classes;
+TRUNCATE TABLE Courses;
+TRUNCATE TABLE Grades;
+TRUNCATE TABLE Attendance;
+
+
+delete Student where student_id in ('601','1','3','11','4');
+
+--Insert the values in the Tables:
+
+--Put Values in Student Table:
+INSERT INTO Student (student_id, student_roll, student_name, student_email, student_password)
+VALUES 
+(001, 33 ,'Anders','anders994@gmail.com','030-0074321'),
+(002, 23 ,'Hussain','hussain94@gmail.com','034-0074321'),
+(031, 33 ,'Sabih','sabih19@gmail.com','030-05074321'),
+(021, 88 ,'Mujtaba','mujtaba3334@gmail.com','03340-0074321'),
+(301, 01 ,'Owais','ovi23@gmail.com','030-007455321'),
+(221, 02 ,'Kashan','kashan99@gmail.com','030-00747321'),
+(051, 07 ,'Zaka','zaka76@gmail.com','030-007437721'),
+(041, 10 ,'Arsal','arsal69@gmail.com','030-00777774321'),
+(441, 30 ,'Omaima','omaima78@gmail.com','030-0074777321'),
+(101, 31 ,'Neha','neha90@gmail.com','030-002274321');
+
+ --Put the values in Courses Table:
+ INSERT INTO Courses(course_code, course_name, course_description, course_start, course_end)
+VALUES 
+(200301, 'Advance Python' ,'Programming','Feb','Dec'),
+(200302, 'Advance SQL' ,'Programming','Jan','Dec'),
+(200304, 'Advance Networking' ,'Programming','April','Dec'),
+(200344, 'Advance BlockChain' ,'Programming','Feb','Sep'),
+(200305, 'Advance Python' ,'Programming','Feb','Oct'),
+(203076, 'Project Mangement' ,'Programming','Feb','Aug'),
+(200325, ' Python' ,'Programming','Feb','Dec'),
+(200441, 'Graphic Desg' ,'Programming','Feb','Dec'),
+(200661, 'MS Office' ,'Programming','July','Dec'),
+(200651, 'Advance Excel' ,'Programming','June','Dec');
+
+ --Put the values in Grades Table:
+
+
+INSERT INTO Grades(student_id, course_code)
+SELECT Student.student_id, Courses.course_code
+FROM Student
+CROSS JOIN Courses; 
+
+
